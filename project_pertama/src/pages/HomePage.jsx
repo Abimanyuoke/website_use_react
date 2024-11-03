aefad292248c861528fc37e86ba257535a5c6603
Abimanyuoke123 <fananiabimanyu@gmail.com>
2024-11-03 09:12:06 +0700

    add homepages

--- project_pertama/src/pages/HomePage.jsx
+++ project_pertama/src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import HeroImage from "../assets/images/hero.svg"
 import AboutImage from "../assets/images/about.svg"
+import Proyek from "../assets/images/dev-gif.gif"
 
 const HomePage = () => {
   return (
@@ -16,7 +17,7 @@ const HomePage = () => {
           </div>
         </div>
 
-        <div className="about grid md:grid-cols-2 md:pt-20 pt-32 gap-20 items-center">
+        <div className="about grid md:grid-cols-2 md:pt-20 pt-32 md:gap-20 gap-10 items-center">
           <div className="box md:order-1 order-2">
             <img src={AboutImage} alt="About Image" className="lg:w-[500px] w-[400px] mx-auto md:m-0"/>
           </div>
@@ -24,6 +25,55 @@ const HomePage = () => {
             <h1 className="lg:text-5xl/tight text-3xl font-medium mb-7">Belajar Membuat Website Dengan <span className="font-bold text-sky-400 underline">Tailwind CSS</span></h1>
             <p className="text-base/loose">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit ullam doloremque beatae corporis odit pariatur enim nostrum explicabo quas repellat?</p>
           </div>
+        </div>
+
+        <div className="services pt-32">
+          <h1 className=" text-center lg:text-5xl/tight text-3xl font-medium mb-2">Layanan</h1>
+          <p className=" text-center text-base/loose">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+          <div className="services-box pt-12 grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6">
+            <div className="box bg-sky-400 rounded-lg shadow p-4">
+              <i className="ri-number-1 text-3xl text-white"></i>
+              <h3 className="text-xl font-bold text-white mt-6 mb-2">Services Name 1</h3>
+              <p className="text-base/loose text-white">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, facere ipsum! Sapiente, veritatis eum. At!</p>
+            </div>
+            <div className="box bg-sky-400 rounded-lg shadow p-4">
+              <i className="ri-number-2 text-3xl text-white"></i>
+              <h3 className="text-xl font-bold text-white mt-6 mb-2">Services Name 2</h3>
+              <p className="text-base/loose text-white">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, facere ipsum! Sapiente, veritatis eum. At!</p>
+            </div>
+            <div className="box bg-sky-400 rounded-lg shadow p-4">
+              <i className="ri-number-3 text-3xl text-white"></i>
+              <h3 className="text-xl font-bold text-white mt-6 mb-2">Services Name 3</h3>
+              <p className="text-base/loose text-white">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, facere ipsum! Sapiente, veritatis eum. At!</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="proyek pt-32">
+        <h1 className=" text-center lg:text-5xl/tight text-3xl font-medium mb-2">Proyek</h1>
+        <p className=" text-center text-base/loose">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+        <div className="proyek-box pt-12 grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6">
+          <div className="box p-2 shadow bg-white">
+            <img src={Proyek} alt="gif coding" />
+            <h3 className="text-xl font-bold text-slate-500 mt-6 mb-2 px-4">Proyek Name 1</h3>
+            <p className="text-base/loose text-slate-500 px-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque quidem beatae quas fugiat ad error!</p>
+          </div>
+          <div className="box p-2 shadow bg-white">
+            <img src={Proyek} alt="gif coding" />
+            <h3 className="text-xl font-bold text-slate-500 mt-6 mb-2 px-4">Proyek Name 2</h3>
+            <p className="text-base/loose text-slate-500 px-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque quidem beatae quas fugiat ad error!</p>
+          </div>
+          <div className="box p-2 shadow bg-white">
+            <img src={Proyek} alt="gif coding" />
+            <h3 className="text-xl font-bold text-slate-500 mt-6 mb-2 px-4">Proyek Name 3</h3>
+            <p className="text-base/loose text-slate-500 px-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque quidem beatae quas fugiat ad error!</p>
+          </div>
+          <div className="box p-2 shadow bg-white">
+            <img src={Proyek} alt="gif coding" />
+            <h3 className="text-xl font-bold text-slate-500 mt-6 mb-2 px-4">Proyek Name 4</h3>
+            <p className="text-base/loose text-slate-500 px-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque quidem beatae quas fugiat ad error!</p>
+          </div>
+        </div>
         </div>
       </div>
     </div>
